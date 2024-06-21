aefb475c5f4be7a71a3d0a31acca022158e55c33
Sérgio da Rocha <113067613+SergioRochaJr@users.noreply.github.com>
2024-06-21 09:49:42 -0300

    restaurando app.js

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ function exibirTextoNaTela(tag, texto) {
 
 }
 
-exibirMensagemInicial();
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
